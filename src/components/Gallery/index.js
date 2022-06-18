ea28fe9b791799dcf800fbf79bbf6fb5dc87a0dd
Kyle Richnafsky <7krich@gmail.com>
2022-06-17 21:45:24 -0400

    image opacity change on hover

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,16 +2,25 @@ import React from 'react'
 
 function Gallery () {
 
+    // new tab function
+    const openInNewTab = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
+
     return (
         <div>
-            <div>
-                <img 
+            <div className="img-container">
+            <a className="link" href="https://limitless-journey-42984.herokuapp.com/">
+                <img  
                     src={require(`../../assets/images/fullstack/0.jpg`)}
-                    id="gallery-pics-large"
-                    className="gallery-pics"
+                    className="gallery-pics-large"
                     alt= "The B word"
                     key= "The B word"
                 />
+                </a>
+                <button className="photo-btn" onClick={() => openInNewTab('https://limitless-journey-42984.herokuapp.com/')}>
+                VIEW
+                </button>
             </div>
         </div>
     )
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Gallery from '../Gallery'
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import { useState } from 'react'
+
 
 function Projects (props) {
     const { currentCategory } = props;
-    const [showGallery, setShowGallery] = useState(false)
+    // const [showGallery, setShowGallery] = useState(false)
     return(
         <section>
             <div className="title-containter">
                 <h1 className="page-title-text">{capitalizeFirstLetter(currentCategory.name)}</h1>
             </div>
             <p>{currentCategory.description}</p> 
-            
-            <button onClick={() => {setShowGallery(true)}}>Show Gallery</button>
-            <button onClick={() => {setShowGallery(false)}}>Hide Gallery</button>
-            { showGallery && <Gallery category={currentCategory.name}/>}
+            <Gallery></Gallery>
+            {/* <button onClick={() => {setShowGallery(true)}}>Show Gallery</button> */}
+            {/* <button onClick={() => {setShowGallery(false)}}>Hide Gallery</button> */}
+            {/* { showGallery && <Gallery category={currentCategory.name}/>} */}
                
                
 
