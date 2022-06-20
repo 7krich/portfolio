2a363af25997c8ac69e4eab216204f050fa23922
Kyle Richnafsky <7krich@gmail.com>
2022-06-20 15:28:46 -0400

    added footer & styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { AiFillGithub, AiFillInstagram } from 'react-icons/ai';
+import { BsLinkedin } from 'react-icons/bs';
 
 
 function Footer () {
-    // return (
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Created by Kyle Richnafsky</p>
 
-    // )
+                <div className="link-div">
+                    <a href="https://github.com/7krich">
+                        <AiFillGithub></AiFillGithub>
+                    </a>
+                </div>
+
+                <div className="link-div">
+                    <a href="https://www.linkedin.com/in/kyle-richnafsky-793715122/">
+                        <BsLinkedin></BsLinkedin>
+                    </a>
+                </div>
+
+                <div className="link-div">
+                    <a href="https://www.instagram.com/k_rich4/?hl=en">
+                        <AiFillInstagram></AiFillInstagram>
+                    </a>
+                </div>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer;
