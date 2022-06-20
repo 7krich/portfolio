67ca3528e8dea48d3427a904a20278e303bfd930
Kyle Richnafsky <7krich@gmail.com>
2022-06-19 20:24:42 -0400

    set up resume file

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Resume from '../Resume'
 import {capitalizeFirstLetter} from '../../utils/helpers'
 
 function Experience (props) {
@@ -7,6 +8,9 @@ function Experience (props) {
     return(
         <section className="title-containter">
             <h1 className="page-title-text">{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <div className="resumeText">
+                <Resume></Resume>
+            </div>
         </section>
     );
 };
